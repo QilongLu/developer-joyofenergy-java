b3d082e4b6680ae99eaec6022a011f581b498ee8
qilong.lu <kecvin_lql1024@outlook.com>
2023-04-06 13:42:50 +0800

    [#2345][qilong.lu] refactor: do unit test for MeterReadingCostController.

--- src/test/java/uk/tw/energy/controller/MeterReadingCostControllerTest.java
+++ src/test/java/uk/tw/energy/controller/MeterReadingCostControllerTest.java
@@ -1,48 +1,51 @@
 package uk.tw.energy.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import uk.tw.energy.domain.ElectricityReading;
-import uk.tw.energy.service.MeterReadingService;
+import uk.tw.energy.service.MeterReadingCostService;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class MeterReadingCostControllerTest {
-    private static final String SMART_METER_ID = "10101010";
-    private final MeterReadingService meterReadingService;
-    private final MeterReadingCostController meterReadingCostController;
-
-    MeterReadingCostControllerTest(MeterReadingService meterReadingService, MeterReadingCostController meterReadingCostController) {
-        this.meterReadingService = meterReadingService;
-        this.meterReadingCostController = meterReadingCostController;
-    }
-
+    private static final String SMART_METER_ID = "smart-meter-0";
+    @MockBean
+    private MeterReadingCostService meterReadingCostService;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    public void givenMeterIdShouldReturnLastWeekCostUsage() {
+    void givenMeterIdShouldReturnLastWeekCostUsage() throws Exception {
         Instant now = Instant.now();
-        ElectricityReading lastWeekReading = new ElectricityReading(now.minus(8,
+        ElectricityReading lastWeekReading = new ElectricityReading(now.minus(7,
                 ChronoUnit.DAYS),
                 BigDecimal.valueOf(0.5));
-        ElectricityReading yesterdayReading = new ElectricityReading(now.minus(1,
-                ChronoUnit.DAYS),
-                BigDecimal.valueOf(0.8));
-        ElectricityReading todayReading = new ElectricityReading(now, BigDecimal.valueOf(1.2));
-        List<ElectricityReading> readings = Arrays.asList(lastWeekReading, yesterdayReading, todayReading);
-        meterReadingService.storeReadings(SMART_METER_ID, readings);
+        List<ElectricityReading> readings = List.of(lastWeekReading);
 
-        ResponseEntity<List<ElectricityReading>> result = meterReadingCostController.getLastWeekReadings(SMART_METER_ID);
+        when(meterReadingCostService.getLastWeekReadings(SMART_METER_ID)).thenReturn(Optional.of(readings));
 
-        List<ElectricityReading> lastWeekReadings = result.getBody();
-        assert lastWeekReadings != null;
-        assertEquals(1, lastWeekReadings.size());
-        assertEquals(lastWeekReading, lastWeekReadings.get(0));
+        mockMvc.perform(MockMvcRequestBuilders.get("/readings/last-week/" + SMART_METER_ID))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].time", equalTo(now.minus(7, ChronoUnit.DAYS).toString())))
+                .andExpect(jsonPath("$.[0].reading").value(0.5));
     }
 }
