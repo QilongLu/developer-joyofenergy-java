18990ce5053f206ca14f0bca08501dec9b34d879
qilong.lu <kecvin_lql1024@outlook.com>
2023-04-12 15:41:29 +0800

    [#2345-1][qilong.lu] feat: implement happy path for getting a day of last week usage costs

--- src/test/java/uk/tw/energy/controller/MeterReadingCostControllerTest.java
+++ src/test/java/uk/tw/energy/controller/MeterReadingCostControllerTest.java
@@ -28,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MeterReadingCostControllerTest {
     private static final String SMART_METER_ID = "smart-meter-0";
     private static final String UNKNOWN_ID = "unknown-meter";
+    private static final String TEST_DATE_STR = "2023-04-12";
     @MockBean
     private MeterReadingCostService meterReadingCostService;
     @Autowired
@@ -62,5 +63,14 @@ class MeterReadingCostControllerTest {
                 )
                 .andExpect(status().isNotFound())
                 .andExpect(content().string(containsString("No price plan matched with " + SMART_METER_ID)));
+    }
+
+    @Test
+    void shouldReturnLastWeekCostOfTheGivenDate() throws Exception {
+        when(meterReadingCostService.getLastWeekCostOfTheDate(SMART_METER_ID, TEST_DATE_STR)).thenReturn(BigDecimal.valueOf(100.0));
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/smart-meters/" + SMART_METER_ID +"/"+ TEST_DATE_STR+ "/last-week/costs"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("100.0"));
     }
 }
